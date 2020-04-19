bcfa39f9bc64047ab66dac9e23ec1c5aac00ef0b
BeataWalo <bw81958@sgh.waw.pl>
2020-04-19 11:07:10 +0200

    exercise done

--- src/main/java/sgh/StockData.java
+++ src/main/java/sgh/StockData.java
@@ -1,22 +1,44 @@
 package sgh;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class StockData {
 
     public static void getAndProcessChange(String stock) throws IOException {
         String filePath = "data_in/" + stock + ".csv";
-        //TODO HINT: You might need to check if the file doesn't already exist...
-        download("https://query1.finance.yahoo.com/v7/finance/download/" + stock +
-                                "?period1=1554504399&period2=1586126799&interval=1d&events=history",
-                        filePath);
+        File inFile = new File(filePath);
+        if (!inFile.exists()) {
+            download("https://query1.finance.yahoo.com/v7/finance/download/" + stock +
+                            "?period1=1554504399&period2=1586126799&interval=1d&events=history",
+                    filePath);
+        }
 
-        //TODO Your code here
+        Scanner sc = new Scanner(inFile);
+        String line = sc.nextLine();
+
+        //creating a new file
+        FileWriter outFile = new FileWriter("data_out/" + stock + ".csv");
+        outFile.write(line + ",Change(in %)" + "\n");
+
+        //calculating value of change
+        while (sc.hasNextLine()) {
+            line = sc.nextLine();
+            String[] lineElem = line.split(",");
+            double open = Double.valueOf(lineElem[1]);
+            double close = Double.valueOf(lineElem[4]);
+            double change = (close - open) / open;
+
+            //entering next lines with the value of change
+            outFile.write(line + "," + change * 100 + "\n");
+        }
+        outFile.close();
     }
 
     public static void download(String url, String fileName) throws IOException {
